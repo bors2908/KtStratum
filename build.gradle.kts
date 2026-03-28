7f2f314607cf8c7c79314dab8794c96e2703c9a3
Boris Ivanov <bors2908@gmail.com>
2026-03-28 21:15:34 +0300

    Add sources to publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,9 @@ version = "1.0.0-SNAPSHOT"
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-}
 
+    withSourcesJar()
+}
 repositories {
     mavenCentral()
 }
